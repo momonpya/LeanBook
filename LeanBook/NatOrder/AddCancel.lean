fd08c96216adcfbb3b97e507f0b6f0c881824302
Momo <braveman_l_brid@yahoo.co.jp>
2026-09-04 04:51:11 +0000

    doing

--- LeanBook/NatOrder/AddCancel.lean
+++ LeanBook/NatOrder/AddCancel.lean
@@ -1,1 +1,29 @@
 import LeanBook.NatSemiring.Distrib
+
+example (n : MyNat) : MyNat.succ n ≠ MyNat.zero := by
+  intro h
+  injection h
+
+example (m n : MyNat) (h : MyNat.succ m = MyNat.succ n) : m = n := by
+  injection h
+
+example (m n : MyNat) (h : m + 1 = n + 1) : m = n := by
+  injection h
+
+variable {l m n : MyNat}
+
+theorem MyNat.add_right_cancel (h : l + m = n + m) : l = n := by
+  induction m with
+  | zero => simp_all
+  | succ m ih =>
+    have lem : (l + m) + 1 = (n + m) + 1 := calc
+      _ = l + (m + 1) := by ac_rfl
+      _ = n + (m + 1) := by rw [h]
+      _ = (n + m) + 1 := by ac_rfl
+    have : l + m = n + m := by
+      injection lem
+    exact ih this
+
+theorem MyNat.add_left_cancel (h : l + m = l + n) : m = n := by
+  rw [MyNat.add_comm l m, MyNat.add_comm l n] at h
+  apply MyNat.add_right_cancel h
